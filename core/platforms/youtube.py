ec0374a91258ee08edf3d0f2c650338a53a4bd14
And73Smi <161311212+And73Smi@users.noreply.github.com>
2026-05-11 22:33:02 +0300

    Update youtube.py

--- core/platforms/youtube.py
+++ core/platforms/youtube.py
@@ -3,32 +3,25 @@ import subprocess
 
 def get_youtube_stream(video_id):
     try:
-        # Ορίζουμε έναν έγκυρο browser User-Agent
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-        
         command = [
             'yt-dlp',
             '--quiet',
             '--no-warnings',
-            '--user-agent', user_agent,
-            '--referer', 'https://youtube.com',
+            '--impersonate', 'chrome',  
+            '--cookies-from-browser', 'chrome', 
+            '-f', 'b',
             '-g',
-            f'https://youtube.comwatch?v={video_id}' # Διορθωμένο URL
+            f'https://www.youtube.com/watch?v={video_id}'
         ]
         
         result = subprocess.run(command, capture_output=True, text=True, check=True)
-        stream_url = result.stdout.strip()
+        stream_url = result.stdout.strip().split('\n')
         
         if stream_url:
-            print(stream_url)
-        else:
-            sys.exit(1)
-            
+            print(stream_url[0])
     except subprocess.CalledProcessError:
         sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         get_youtube_stream(sys.argv[1])
-    else:
-        sys.exit(1)
